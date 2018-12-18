6fe38d0ba96cd52253b8a2ea62074e6728cda5de
hkulas <hubertzkulas@gmail.com>
2018-12-18 22:01:23 +0100

    Initial commit

---
+++ src/main/java/com/hubertkulas/backendpatronage/BackendPatronageApplication.java
@@ -0,0 +1,14 @@
+package com.hubertkulas.backendpatronage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPatronageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendPatronageApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/hubertkulas/backendpatronage/BackendPatronageApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hubertkulas.backendpatronage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendPatronageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
