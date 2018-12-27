ff9499c0386820b437a2fa1169f8ad074a110b25
hkulas <hubertzkulas@gmail.com>
2018-12-27 20:37:48 +0100

    db update

--- src/main/java/com/hubertkulas/backendpatronage/service/DataBaseLoader.java
+++ src/main/java/com/hubertkulas/backendpatronage/service/DataBaseLoader.java
@@ -69,30 +69,5 @@ public class DataBaseLoader implements CommandLineRunner {
                 LocalDateTime.parse("2018-07-15 16:30", formatter)));
 
 
-//        conferenceRoomReservationRepository.save(
-//                new ConferenceRoomReservation("1", LocalDateTime.parse("1996-12-14 20:50", formatter), LocalDateTime.parse("1996-12-14 21:50", formatter),
-//                new ConferenceRoom("BlueRoom", "10", 1, true, 20, 30, 0),
-//                new ConferenceRoomEquipment("Alfa", true, 56, "+12 687123123"),
-//                new Organization("Backend Java")));
-//
-//        conferenceRoomReservationRepository.save(
-//                new ConferenceRoomReservation("2", LocalDateTime.parse("2018-07-14 14:00", formatter), LocalDateTime.parse("2018-07-14 15:00", formatter),
-//                new ConferenceRoom("BlueWall", "20", 2, true, 20, 10, 0),
-//                new ConferenceRoomEquipment("Beta", true, 31, "+14 123456789"),
-//                new Organization("Backend .Net")));
-//
-//        conferenceRoomReservationRepository.save(
-//                new ConferenceRoomReservation("3", LocalDateTime.parse("2018-09-14 10:00", formatter), LocalDateTime.parse("2018-09-14 18:00", formatter),
-//                new ConferenceRoom("OrangeRoom", "22", 2, true, 15, 5, 0),
-//                new ConferenceRoomEquipment("Gamma", true, 12, "+12 147103103"),
-//                new Organization("Frontend")));
-//
-//        conferenceRoomReservationRepository.save(
-//                new ConferenceRoomReservation("4", LocalDateTime.parse("2018-07-15 16:00", formatter), LocalDateTime.parse("2018-07-15 16:30", formatter),
-//                new ConferenceRoom("WideRoom", "40", 4, false, 15, 8, 6),
-//                new ConferenceRoomEquipment("Delta", false, 99, "+12 999999999"),
-//                new Organization("QA")));
-
-
     }
 }
