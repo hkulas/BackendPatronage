0e3fb95adb0084f3c13f2ed2203bceb492ca9bec
hkulas <hubertzkulas@gmail.com>
2019-01-18 17:11:27 +0100

    Added IntegrationTest

--- src/main/java/com/hubertkulas/backendpatronage/repository/ConferenceRoomReservationRepository.java
+++ src/main/java/com/hubertkulas/backendpatronage/repository/ConferenceRoomReservationRepository.java
@@ -3,5 +3,5 @@ package com.hubertkulas.backendpatronage.repository;
 import com.hubertkulas.backendpatronage.model.ConferenceRoomReservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ConferenceRoomReservationRepository extends JpaRepository<ConferenceRoomReservation,Long> {
+public interface ConferenceRoomReservationRepository extends JpaRepository<ConferenceRoomReservation, Long> {
 }
---
+++ src/test/java/com/hubertkulas/backendpatronage/ConferenceRoomReservationRepositoryIntegrationTest.java
@@ -0,0 +1,38 @@
+package com.hubertkulas.backendpatronage;
+
+import com.hubertkulas.backendpatronage.model.ConferenceRoomReservation;
+import com.hubertkulas.backendpatronage.repository.ConferenceRoomReservationRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ConferenceRoomReservationRepositoryIntegrationTest {
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private ConferenceRoomReservationRepository conferenceRoomReservationRepository;
+
+    @Test
+    public void returnConferenceRoomReservationMock_whenGetOneIsCalled() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        ConferenceRoomReservation conferenceRoomReservationMock = new ConferenceRoomReservation("23", LocalDateTime.parse("1996-12-14 20:50", formatter),
+                LocalDateTime.parse("1996-12-14 21:50", formatter));
+        conferenceRoomReservationRepository.save(conferenceRoomReservationMock);
+        ConferenceRoomReservation conferenceRoomReservationCall = conferenceRoomReservationRepository.getOne(1l);
+        assertThat(conferenceRoomReservationCall)
+                .isEqualTo(conferenceRoomReservationMock);
+    }
+
+
+}
